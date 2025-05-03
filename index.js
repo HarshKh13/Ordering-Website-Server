f3e55aec5c067be5dfa4a233de03a2f92a78003f
HarshKh13 <harshkhanna1309@gmail.com>
2025-05-03 03:40:18 -0400

    cors issue

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const databaseConnection = require('./databaseConnection');
 const items = require('./routes/items');
 const cartItems = require('./routes/cartItems')
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use('/api', items);
 app.use('/api', cartItems);
